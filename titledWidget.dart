1d208f555c82e1236cf4810281b5b761f59f93cd
Ibtesam Mahmood <ibtesam.mahmood2@gmail.com>
2019-05-18 18:15:55 -0400

    minor update

--- titledWidget.dart
+++ titledWidget.dart
@@ -3,10 +3,16 @@ import 'package:flutter/material.dart';
 class TitledWidget extends StatelessWidget {
   final String text;
   final Widget widget;
+  final TextStyle headerStyle;
   final MainAxisAlignment mainAxisAlignment;
   final CrossAxisAlignment crossAxisAlignment;
   
-  TitledWidget(this.text, {this.widget, this.mainAxisAlignment = MainAxisAlignment.center, this.crossAxisAlignment = CrossAxisAlignment.start});
+  TitledWidget(
+    this.text,
+    {this.widget, this.mainAxisAlignment = MainAxisAlignment.center, this.crossAxisAlignment = CrossAxisAlignment.start,
+    this.headerStyle
+    }
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +20,7 @@ class TitledWidget extends StatelessWidget {
       mainAxisAlignment: this.mainAxisAlignment,
       crossAxisAlignment: this.crossAxisAlignment,
       children: <Widget>[
-        Text(text),
+        Text(text, style: headerStyle,),
         widget
       ],
     );
